d78bd08a98bc81709216a2d2fd45024569d5c46e
sujithreddydharmareddy <dharmareddysujith15@gmail.com>
2021-11-27 17:53:44 -0600

    commit 9

--- frontend-react/src/Identifypage.js
+++ frontend-react/src/Identifypage.js
@@ -130,6 +130,25 @@ const Identify = () => {
           </FormControl>
         </div>
       </div>
+      <div
+        style={{
+          border: "solid black",
+          backgroundColor: "white",
+          padding: "2rem",
+          width: "80%",
+          textAlign: "center",
+        }}
+      >
+        <h4 id="placeholder1">Upload some data to get started...</h4>
+
+        <div
+          style={{
+            width: "100%",
+            height: "30rem",
+            visibility: "hidden",
+          }}
+        ></div>
+      </div>
     </div>
   );
 };
